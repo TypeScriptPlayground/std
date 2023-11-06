22aed27fc9cbc4e3e537534d56373b8f87eab87c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-11-06 15:45:19 +0100

    Update average.ts

--- src/math/average.ts
+++ src/math/average.ts
@@ -5,6 +5,6 @@
  * @param list List of number elements
  * @returns number, the average amount from the number array
  */
-export function average(list : number[]) : number {
+export default function average(list : number[]) : number {
     return list.reduce((last, current) => last + current, 0) / list.length || 0;
 }
