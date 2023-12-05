f8660053f68b9966f18574c49f428bccc0564cb2
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-12-05 17:22:12 +0100

    Update parse.test.ts

--- src/cli/parse.test.ts
+++ src/cli/parse.test.ts
@@ -1,1 +1,42 @@
 import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import parse from "./parse.ts";
+
+Deno.test(
+    'Parsing CLI string arguments.',
+    (test) => {
+        test.step({
+            name: 'Bool arguments (--argument)',
+            fn: () => {
+                
+            }
+        })
+
+        test.step({
+            name: 'Short bool arguments (-a)',
+            fn: () => {
+
+            }
+        })
+
+        test.step({
+            name: 'Multiple short bool arguments (-abc)',
+            fn: () => {
+
+            }
+        })
+
+        test.step({
+            name: 'Value Arguments (--argument=value)',
+            fn: () => {
+
+            }
+        })
+
+        test.step({
+            name: 'List arguments (--argument:value1,value2)',
+            fn: () => {
+
+            }
+        })
+    }
+)
