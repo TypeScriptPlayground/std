82b71e598e7e1509ae4a4543451bf127221c78af
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-12-05 16:51:01 +0100

    Added module exports

---
+++ src/filesystem/mod.ts
@@ -0,0 +1,11 @@
+import getFileExtension from "./get_file_extension.ts"
+import isDirectory from "./is_directory.ts"
+import isFile from "./is_file.ts"
+import normalizePath from "./normalize_path.ts"
+
+export {
+    getFileExtension,
+    isDirectory,
+    isFile,
+    normalizePath
+}
