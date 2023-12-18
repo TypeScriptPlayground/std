9f503247d5ac91741be4ba62070b7a6d5b496581
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-12-18 13:28:01 +0100

    Files renamed

--- src/filesystem/is_path_to_file.ts
+++ src/filesystem/is_path_to_file.ts
@@ -1,3 +1,4 @@
 export default function isFile() {
     
 }
+Deno.lstat(path)
--- src/filesystem/mod.ts
+++ src/filesystem/mod.ts
@@ -1,11 +1,11 @@
 import getFileExtension from "./get_file_extension.ts"
-import isDirectory from "./is_directory.ts"
-import isFile from "./is_file.ts"
+import isPathToDirectory from "./is_path_to_directory.ts"
+import isFile from "./is_path_to_file.ts"
 import normalizePath from "./normalize_path.ts"
 
 export {
     getFileExtension,
-    isDirectory,
+    isPathToDirectory,
     isFile,
     normalizePath
 }
