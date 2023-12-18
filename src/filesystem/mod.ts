106074a45f73e43b5a4c79c8f8c3ae120e36c8e8
Mqxx <62719703+Mqxx@users.noreply.github.com>
2023-12-18 13:38:37 +0100

    Update mod.ts

--- src/filesystem/mod.ts
+++ src/filesystem/mod.ts
@@ -1,11 +1,11 @@
 import getFileExtensionFromPath from "./get_file_extension_from_path.ts"
 import isPathToDirectory from "./is_path_to_directory.ts"
-import isFile from "./is_path_to_file.ts"
+import isPathToFile from "./is_path_to_file.ts"
 import normalizePath from "./normalize_path.ts"
 
 export {
     getFileExtensionFromPath,
     isPathToDirectory,
-    isFile,
+    isPathToFile,
     normalizePath
 }
