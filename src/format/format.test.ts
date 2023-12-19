5cd563032448bfbb2ccb860f448771ce78ee979d
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-12-19 15:54:00 +0100

    Update format.test.ts

--- src/format/format.test.ts
+++ src/format/format.test.ts
@@ -1,1 +1,32 @@
+import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import format from "./format.ts";
 
+Deno.test(
+    'Format string.',
+    async (test) => {
+        await test.step({
+            name: 'Normal template string.',
+            fn: () => {
+                assertEquals(
+                    format(
+                        'Test, hello {{person}}',
+                        {person: 'User'}
+                    ),
+                    'Test, hello User'
+                )
+            }
+        })
+        await test.step({
+            name: 'Object does not exist.',
+            fn: () => {
+                assertEquals(
+                    format(
+                        'Test, hello {{keyDoesNotExist}}',
+                        {person: 'User'}
+                    ),
+                    new KeyError();
+                )
+            }
+        })
+    }
+)
