bc23e11e8e9ccee322e6fb49605c49f93f3ea17e
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-02 15:39:18 +0100

    Update mod.ts

--- src/math/mod.ts
+++ src/math/mod.ts
@@ -1,9 +1,11 @@
 import average from "./average.ts";
 import map from "./map.ts";
 import median from "./median.ts";
+import toFixed from "./to_fixed.ts";
 
 export { 
     average,
     map,
-    median
+    median,
+    toFixed
 }
