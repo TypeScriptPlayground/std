728f2fec101810ba9a7d4c3b9d8b15e7947ac52c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-02 13:49:24 +0100

    Update to_fixed.ts

--- src/math/to_fixed.ts
+++ src/math/to_fixed.ts
@@ -2,7 +2,7 @@
 export default function toFixed(
     value : number,
     precision : number,
-    handleTrailingDecimalPlaces : (value : number) => number = Math.round
+    handleTrailingDecimalPlaces : (decimalPlacesValue : number) => number = Math.round
 ) {
     const decimalPlaces = Math.pow(10, precision)
     return (handleTrailingDecimalPlaces(value * decimalPlaces)) / decimalPlaces;
