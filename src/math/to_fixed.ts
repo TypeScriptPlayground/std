9fed8225c8589f22e051a2032a05b11e39729399
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-03 15:55:20 +0100

    JSDoc fix

--- src/math/to_fixed.ts
+++ src/math/to_fixed.ts
@@ -1,4 +1,12 @@
-/** */
+/**
+ * This function removes decimal places based on the `precision`. No string conversion is used! By default `Math.round`
+ * is used for the last decimal place.
+ * 
+ * @param value The number for which the decimal places are to be removed
+ * @param precision The number of decimal places
+ * @param [handleTrailingDecimalPlaces=Math.round] The function for calculating the last decimal place 
+ * @returns The converted number
+ */
 export default function toFixed(
     value : number,
     precision : number,
