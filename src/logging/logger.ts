5311d9a8fa5d1905599c7a8567ad9d1234db16e6
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-08 09:13:43 +0100

    Update logger.ts

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,7 +1,13 @@
 import Loggers from "./loggers.ts";
 
 export default class Logger {
-    constructor(loggers : Loggers) {
+    constructor(loggers : Loggers = {
+        log: console.log,
+        info: console.info,
+        warn: console.warn,
+        error: console.error,
+        file:
+    }) {
 
     }
 
