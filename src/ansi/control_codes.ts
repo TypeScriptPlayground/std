7864fafb089ca78916aec5ec51117ac274242903
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-11 11:01:48 +0100

    Update control_codes.ts

--- src/ansi/control_codes.ts
+++ src/ansi/control_codes.ts
@@ -4,69 +4,69 @@ import { TypeValuesFromObject } from "../type/mod.ts";
  * This object contains all ANSI control codes (`0x00`-`0x1F`)
  */
 const controlCodes = {
-    /** Null (`\x00`) */
+    /** Null (`0x00`) */
     NUL: '\x00',
-    /** Start of heading (`\x01`) */
+    /** Start of heading (`0x01`) */
     SOH: '\x01',
-    /** Start of text (`\x02`) */
+    /** Start of text (`0x02`) */
     STX: '\x02',
-    /** End of text (`\x03`) */
+    /** End of text (`0x03`) */
     ETX: '\x03',
-    /** End of transmission (`\x04`) */
+    /** End of transmission (`0x04`) */
     EOT: '\x04',
-    /** Enquiry  (`\x05`) */
+    /** Enquiry  (`0x05`) */
     ENQ: '\x05',
-    /** Acknowledge  (`\x06`) */
+    /** Acknowledge  (`0x06`) */
     AOK: '\x06',
-    /** Bell (`\x07`) */
+    /** Bell (`0x07`) */
     BEL: '\x07',
-    /** Backspace (`\x08`) */
+    /** Backspace (`0x08`) */
     BS: '\x08',
-    /** Horizontal tab (`\x09`) */
+    /** Horizontal tab (`0x09`) */
     TAB: '\x09',
-    /** NL line feed, new Line (`\x0A`) */
+    /** NL line feed, new Line (`0x0A`) */
     LF: '\x0A',
-    /** Vertical tab (`\x0B`) */
+    /** Vertical tab (`0x0B`) */
     VT: '\x0B',
-    /** NP form feed, new page (`\x0C`) */
+    /** NP form feed, new page (`0x0C`) */
     FF: '\x0C',
-    /** Carriage return (`\x0D`) */
+    /** Carriage return (`0x0D`) */
     CR: '\x0D',
-    /** Shift out (`\x0E`) */
+    /** Shift out (`0x0E`) */
     SO: '\x0E',
-    /** Shift in (`\x0F`) */
+    /** Shift in (`0x0F`) */
     SI: '\x0F',
-    /** Data link escape (`\x10`) */
+    /** Data link escape (`0x10`) */
     DLE: '\x10',
-    /** Device control I (`\x11`) */
+    /** Device control I (`0x11`) */
     DC1: '\x11',
-    /** Device control 2 (`\x12`) */
+    /** Device control 2 (`0x12`) */
     DC2: '\x12',
-    /** Device control 3 (`\x13`) */
+    /** Device control 3 (`0x13`) */
     DC3: '\x13',
-    /** Device control 4 (`\x14`) */
+    /** Device control 4 (`0x14`) */
     DC4: '\x14',
-    /** Negative acknowledge (`\x15`) */
+    /** Negative acknowledge (`0x15`) */
     NAK: '\x15',
-    /** Synchronous idle (`\x16`) */
+    /** Synchronous idle (`0x16`) */
     SYN: '\x16',
-    /** End of trans. block (`\x17`) */
+    /** End of trans. block (`0x17`) */
     ETB: '\x17',
-    /** Cancel (`\x18`) */
+    /** Cancel (`0x18`) */
     CAN: '\x18',
-    /** End of medium (`\x19`) */
+    /** End of medium (`0x19`) */
     EM: '\x19',
-    /** Substitute (`\x1A`) */
+    /** Substitute (`0x1A`) */
     SUB: '\x1A',
-    /** Escape (`\x1B`) */
+    /** Escape (`0x1B`) */
     ESC: '\x1B',
-    /** File separator (`\x1C`) */
+    /** File separator (`0x1C`) */
     FS: '\x1C',
-    /** Group separator (`\x1D`) */
+    /** Group separator (`0x1D`) */
     GS: '\x1D',
-    /** Record separator (`\x1E`) */
+    /** Record separator (`0x1E`) */
     RS: '\x1E',
-    /** Unit separator (`\x1F`) */
+    /** Unit separator (`0x1F`) */
     US: '\x1F',
 } as const;
 
