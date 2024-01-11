12b9da48ab7d02ab479ab65faabb6bbd15d92b44
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-11 12:03:06 +0100

    Control Characters Updated

---
+++ src/ansi/control_codes/c1_controls.ts
@@ -0,0 +1,82 @@
+import { TypeValuesFromObject } from "../../type/mod.ts";
+
+/**
+ * This object contains all C1 ANSI control codes.
+ * 
+ * [Reference](https://en.wikipedia.org/wiki/C0_and_C1_control_codes#C1_controls)
+ */
+const c1Controls = {
+    /** Padding Character (`0x80`) */
+    PAD: '\x80',
+    /** High Octet Preset (`0x81`) */
+    HOP: '\x81',
+    /** Break Permitted Here (`0x82`) */
+    BPH: '\x82',
+    /** No Break Here (`0x83`) */
+    NBH: '\x83',
+    /** Index (`0x84`) */
+    IND: '\x84',
+    /** Next Line (`0x85`) */
+    NEL: '\x85',
+    /** Start of Selected Area (`0x86`) */
+    SSA: '\x86',
+    /** End of Selected Area (`0x87`) */
+    ESA: '\x87',
+    /** Horizontal Tabulation Set (`0x88`) */
+    HTS: '\x88',
+    /** Horizontal Tabulation With Justification (`0x89`) */
+    HTJ: '\x89',
+    /** Vertical Tabulation Set (`0x8A`) */
+    VTS: '\x8A',
+    /** Partial Line Down (`0x8B`) */
+    PLD: '\x8B',
+    /** Partial Line Up (`0x8C`) */
+    PLU: '\x8C',
+    /** Reverse Index (`0x8D`) */
+    RI: '\x8D',
+    /** Single-Shift 2 (`0x8E`) */
+    SS2: '\x8E',
+    /** Single-Shift 3 (`0x8F`) */
+    SS3: '\x8F',
+    /** Device Control String (`0x90`) */
+    DCS: '\x90',
+    /** Private Use 1 (`0x91`) */
+    PU1: '\x91',
+    /** Private Use 2 (`0x92`) */
+    PU2: '\x92',
+    /** Set Transmit State (`0x93`) */
+    STS: '\x93',
+    /** Cancel character (`0x94`) */
+    CCH: '\x94',
+    /** Message Waiting (`0x95`) */
+    MW: '\x95',
+    /** Start of Protected Area (`0x96`) */
+    SPA: '\x96',
+    /** End of Protected Area (`0x97`) */
+    EPA: '\x97',
+    /** Start of String (`0x98`) */
+    SOS: '\x98',
+    /** Single Graphic Character Introducer (`0x99`) */
+    SGC: '\x99',
+    /** Single Character Introducer (`0x9A`) */
+    SCI: '\x9A',
+    /** Control Sequence Introducer (`0x9B`) */
+    CSI: '\x9B',
+    /** String Terminator (`0x9C`) */
+    ST: '\x9C',
+    /** Operating System Command (`0x9D`) */
+    OSC: '\x9D',
+    /** Privacy Message (`0x9E`) */
+    PM: '\x9E',
+    /** Application Program Command (`0x9F`) */
+    APC: '\x9F',
+
+} as const;
+
+export default c1Controls
+
+/**
+ * This type gets generated from the values of {@linkcode c1Controls}.
+ */
+export type C1Control = TypeValuesFromObject<typeof c1Controls>
+
--- src/ansi/mod.ts
+++ src/ansi/mod.ts
@@ -1,5 +1,13 @@
-import controlCodes from "./control_codes.ts";
-import { ControlCode } from "./control_codes.ts";
+import C0Controls from "./control_codes/c0_controls.ts";
+import C1Controls from "./control_codes/c1_controls.ts";
+import { C0Control } from "./control_codes/c0_controls.ts";
+import { C1Control } from "./control_codes/c1_controls.ts";
 
-export { controlCodes }
-export type { ControlCode }
+export {
+    C0Controls,
+    C1Controls
+}
+export type {
+    C0Control,
+    C1Control
+}
