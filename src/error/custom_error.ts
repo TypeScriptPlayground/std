c2d6fc72acc52a4616bb5d393077de94a74063b0
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-11 09:00:06 +0100

    Create custom_error.ts

---
+++ src/error/custom_error.ts
@@ -0,0 +1,16 @@
+/**
+ * This error class provides the default behavior of the base Error class when inheriting,
+ * but takes into account the inheritance of the types.
+ * This allows you to check `error instanceof CustomError`.
+ * The error class suppresses the correct inheritance of the prototype if you inherit directly from it. 
+ * inherits directly from it
+ *
+ * @param message The custom error message
+ * @returns The custom error
+ */
+export class CustomError extends Error {
+  constructor(message : string) {
+    super(message);
+    Object.setPrototypeOf( this, new.target.prototype );
+  }
+}
