5b0840fc97a26086b599801587746ee7e8c5f0b3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-11 14:21:31 +0100

    Create all_controls.ts

---
+++ src/ansi/control_codes/all_controls.ts
@@ -0,0 +1,9 @@
+import c0Controls from "./c0_controls.ts";
+import c1Controls from "./c1_controls.ts";
+
+const allControls = {
+    ...c0Controls,
+    ...c1Controls
+}
+
+export default allControls;
