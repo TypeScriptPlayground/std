208731a1e8cc6725afd4f56921382dd9f0ed6b7b
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-15 14:31:10 +0100

    Refactoring

--- src/ansi/mod.ts
+++ src/ansi/mod.ts
@@ -1,10 +1,10 @@
-import c0Controls from "./control_codes/c0_controls.ts";
-import c1Controls from "./control_codes/c1_controls.ts";
-import allControls from "./control_codes/all_controls.ts";
+import c0Controls from "./control/c0_controls.ts";
+import c1Controls from "./control/c1_controls.ts";
+import allControls from "./control/all_controls.ts";
 
-import { C0Control } from "./control_codes/c0_controls.ts";
-import { C1Control } from "./control_codes/c1_controls.ts";
-import { AllControl } from "./control_codes/all_controls.ts";
+import { C0Control } from "./control/c0_controls.ts";
+import { C1Control } from "./control/c1_controls.ts";
+import { AllControl } from "./control/all_controls.ts";
 
 export {
     c0Controls,
