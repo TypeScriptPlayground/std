df79ed4106449cde1d59512343b5db1b37a5227d
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-16 11:58:34 +0100

    Update mod.ts

--- src/type/mod.ts
+++ src/type/mod.ts
@@ -1,9 +1,11 @@
 import TypeValuesFromObject from "./type_values_from_object.ts";
 import TypeValuesFromArray from "./type_values_from_array.ts";
 import TypeKeysFromObject from "./type_key_from_object.ts";
+import NumberRange from "./number_range.ts";
 
 export type {
     TypeValuesFromObject,
     TypeValuesFromArray,
-    TypeKeysFromObject
+    TypeKeysFromObject,
+    NumberRange
 }
