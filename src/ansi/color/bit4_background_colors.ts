231e0b12d19ae88b2f136453921e932a98dbc75f
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-16 06:52:29 +0100

    Added Bright Colors

--- src/ansi/color/bit4_background_colors.ts
+++ src/ansi/color/bit4_background_colors.ts
@@ -13,7 +13,15 @@ const bit4BackgroundColors = {
     BLUE: '44',
     MAGENTA: '45',
     CYAN: '46',
-    WHITE: '47'
+    WHITE: '47',
+    BRIGHT_BLACK: '100',
+    BRIGHT_RED: '101',
+    BRIGHT_GREEN: '102',
+    BRIGHT_YELLOW: '103',
+    BRIGHT_BLUE: '104',
+    BRIGHT_MAGENTA: '105',
+    BRIGHT_CYAN: '106',
+    BRIGHT_WHITE: '107'
 } as const;
 
 export default bit4BackgroundColors;
--- src/ansi/color/bit4_foreground_colors.ts
+++ src/ansi/color/bit4_foreground_colors.ts
@@ -13,7 +13,15 @@ const bit4ForegroundColors = {
     BLUE: '34',
     MAGENTA: '35',
     CYAN: '36',
-    WHITE: '37'
+    WHITE: '37',
+    BRIGHT_BLACK: '90',
+    BRIGHT_RED: '91',
+    BRIGHT_GREEN: '92',
+    BRIGHT_YELLOW: '93',
+    BRIGHT_BLUE: '94',
+    BRIGHT_MAGENTA: '95',
+    BRIGHT_CYAN: '96',
+    BRIGHT_WHITE: '97'
 } as const;
 
 export default bit4ForegroundColors;
