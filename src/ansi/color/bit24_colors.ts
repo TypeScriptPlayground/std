f0e05943f36405a98781b72b12e157ec62ae529a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-16 09:30:01 +0100

    Create bit24_colors.ts

---
+++ src/ansi/color/bit24_colors.ts
@@ -0,0 +1,13 @@
+/**
+ * This object contains all 4-Bit color codes.
+ * 
+ * [Wikipedia](https://en.wikipedia.org/wiki/ANSI_escape_code#24-bit)
+ */
+const bit24Colors = {
+    get: () => {
+
+    }
+} as const;
+
+export default bit24Colors;
+
