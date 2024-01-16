833d1e3d9cd4531d5b89339a27bd35131f6a9718
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-16 07:42:03 +0100

    Update mod.ts

--- src/ansi/mod.ts
+++ src/ansi/mod.ts
@@ -1,19 +1,27 @@
 import c0Controls from "./control/c0_controls.ts";
 import c1Controls from "./control/c1_controls.ts";
 import allControls from "./control/all_controls.ts";
+import bit4Colors from "./color/bit4_colors.ts";
+import bit8Colors from "./color/bit8_colors.ts";
 
 import { C0Control } from "./control/c0_controls.ts";
 import { C1Control } from "./control/c1_controls.ts";
 import { AllControl } from "./control/all_controls.ts";
+import { Bit4Color } from "./color/bit4_colors.ts";
+import { Bit8Color } from "./color/bit8_colors.ts";
 
 export {
     c0Controls,
     c1Controls,
-    allControls
+    allControls,
+    bit4Colors,
+    bit8Colors
 }
 
 export type {
     C0Control,
     C1Control,
-    AllControl
+    AllControl,
+    Bit4Color,
+    Bit8Color
 }
