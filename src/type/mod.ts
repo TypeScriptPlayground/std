5ad75be10b69e828903d019d0f02dcf5af7eb416
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-17 06:28:34 +0100

    Renamed

--- src/type/mod.ts
+++ src/type/mod.ts
@@ -1,6 +1,6 @@
 import ValuesFromObject from "./values_from_object.ts";
 import ValuesFromArray from "./values_from_array.ts";
-import KeysFromObject from "./key_from_object.ts";
+import KeysFromObject from "./keys_from_object.ts";
 import NumberRange from "./number_range.ts";
 
 export type {
