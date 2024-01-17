b5eccbae448f3e1efe8675b2bcf01bdd52002010
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-17 07:41:07 +0100

    Create bit24_colors.ts

---
+++ src/ansi/color/bit24_colors.ts
@@ -0,0 +1,11 @@
+import { NumberRange } from "../../type/mod.ts";
+
+/**
+ * .
+ */
+export interface Bit24Color {
+    red : NumberRange<0, 255>,
+    green : NumberRange<0, 255>,
+    blue : NumberRange<0, 255>
+};
+
