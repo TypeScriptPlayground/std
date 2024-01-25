54ad4621b9118cbad9c7e5ea2b75d7d80baf5ffc
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-25 13:34:12 +0100

    Create type_from_unknown.ts

---
+++ src/type/type_from_unknown.ts
@@ -0,0 +1,11 @@
+/**
+ * This function creates a type from the parameter and enforces this type as return type. Use this function to convert
+ * types from `any` or `unknown` to a defined type.
+ * 
+ * @template Type The type to create from the parameter.
+ * @param parameter The parameter which has an `any` or `unknown` type.
+ * @returns The parameter with the new type.
+ */
+export function typeFromUnknown<Type extends unknown>(parameter : Type) : Type {
+    return parameter as Type;
+}
