50e175448054892f45b43c11265e97c87ab83792
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-25 14:22:43 +0100

    Update and rename binary_4bit.ts to dual_4bit.ts

--- src/type/binary/binary_4bit.ts
+++
@@ -1,8 +0,0 @@
-import BinaryDigit from './binary_digit.ts'
-
-/**
- * This type represents 4 bit.
- */
-type Binary4Bit = `${BinaryDigit}${BinaryDigit}${BinaryDigit}${BinaryDigit}`
-
-export default Binary4Bit;
---
+++ src/type/binary/dual_4bit.ts
@@ -0,0 +1,8 @@
+import DualDigit from './dual_digit.ts'
+
+/**
+ * This type represents 4 bit.
+ */
+type Dual4Bit = `${BinaryDigit}${BinaryDigit}${BinaryDigit}${BinaryDigit}`
+
+export default Dual4Bit;
