493208fe08bcbc062e8d3dbfb50af07e05c8c6ff
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-25 14:17:37 +0100

    Create octal_digit.ts

---
+++ src/type/binary/octal_digit.ts
@@ -0,0 +1,14 @@
+/**
+ * This type represents a octal digit.
+ */
+type OctalDigit = 
+    '0' |
+    '1' |
+    '2' |
+    '3' |
+    '4' |
+    '5' |
+    '6' |
+    '7';
+
+export default OctalDigit;
