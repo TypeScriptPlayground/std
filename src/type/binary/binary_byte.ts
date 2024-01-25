91ede8798ebd5f28638c719b5799cc86d9bf08a6
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-25 14:00:11 +0100

    Create binary_byte.ts

---
+++ src/type/binary/binary_byte.ts
@@ -0,0 +1,8 @@
+import BinaryBit from './binary_bit.ts'
+
+/**
+ * This type represents a byte.
+ */
+type BinaryByte = `${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}`
+
+export default BinaryByte
