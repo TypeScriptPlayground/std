2a9a01273c7dea2994f73f6251356f7909a5fdbe
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-25 14:04:21 +0100

    Create binary_4bit.ts

---
+++ src/type/binary/binary_4bit.ts
@@ -0,0 +1,8 @@
+import BinaryBit from './binary_bit.ts'
+
+/**
+ * This type represents 4 bit.
+ */
+type Binary4Bit = `${BinaryBit}${BinaryBit}${BinaryBit}${BinaryBit}`
+
+export default Binary4Bit;
