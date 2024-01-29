cfca50e475c5043faa5dc8979c7e501b6e53a79b
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-29 08:50:21 +0100

    Update normalize_path.ts

--- src/filesystem/normalize_path.ts
+++ src/filesystem/normalize_path.ts
@@ -3,16 +3,42 @@
  * 
  * @example
  * ```
- * normalizePath('this///////is/a//very/messed//./up//.//long/../path//to///a//..../special/../file///.///');
+ * normalizePath('this///////is/a//very/messed//./up//.//long/../path//to///a//../../special/../file///.///');
  * // this/is/a/very/messed/up/path/file/
  * ```
  * @param path The path to normalize
  * @param separator The separator used for the path
+ * @param maxIterations The maximum amount of iterations used when replacing the parent directory (`../`)
  * @returns The normalized path
  */
 export default function normalizePath(
     path : string,
-    separator : string | RegExp
+    separator : '\\' | '\/',
+    maxIterations : number = 200
 ) : string {
+    const matchStackedSeparator = new RegExp(`(?<=${separator})${separator}${separator}`, 'gm');
+    const matchDoubleSeparator = new RegExp(`${separator}${separator}`, 'gm');
+    const matchCurrentDirectory = new RegExp(`(?<!\.)\.${separator}`, 'gm');
+    const matchParentDirectory = new RegExp(`([^${separator}]+${separator})\.\.${separator}`, 'gm');
+    
+    path = path.replace(
+        matchStackedSeparator,
+        ''
+    ).replace(
+        matchDoubleSeparator,
+        separator
+    ).replace(
+        matchCurrentDirectory,
+        ''
+    );
+
+    for (
+        let replaceIteration = 0;
+        path.match(matchParentDirectory) && replaceIteration < maxIterations;
+        replaceIteration++
+    ) {
+        path = path.replace(matchParentDirectory, '')
+    }
+
     return path;
 }
