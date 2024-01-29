66cb8e6cf01dcf8e0193a24107284239c3563270
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-29 07:39:53 +0100

    Update normalize_path.ts

--- src/filesystem/normalize_path.ts
+++ src/filesystem/normalize_path.ts
@@ -1,11 +1,18 @@
 /**
  * This function takes a path and a separator and returns the normalized path.
+ * 
+ * @example
  * ```
  * normalizePath('this///////is/a//very/messed//./up//.//long/../path//to///a//..../special/../file///.///');
  * // this/is/a/very/messed/up/path/file/
  * ```
+ * @param path The path to normalize
+ * @param separator The separator used for the path
  * @returns The normalized path
  */
-export default function normalizePath() {
-    
+export default function normalizePath(
+    path : string,
+    separator : string | RegExp
+) : string {
+    return path;
 }
