5e1517bbef33582f6ae98fd55ef8548944c89afc
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-29 08:50:18 +0100

    Update normalize_path.test.ts

--- src/filesystem/normalize_path.test.ts
+++ src/filesystem/normalize_path.test.ts
@@ -1,2 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
 import normalizePath from "./normalize_path.ts";
+
+Deno.test(
+    'Split path.',
+    async (test) => {
+        await test.step({
+            name: 'Path to file with separator "/".',
+            fn: () => {
+                assertEquals(
+                    normalizePath(
+                        'this///////is/a//very/messed//./up//.//long/../path//to///a//../../special/../file///.///',
+                        '/'
+                    ),
+                    'this/is/a/very/messed/up/path/file/'
+                )
+            }
+        })
+    }
+)
