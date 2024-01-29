ad74d70e7a1608866e1f60ab855bd9d96b80fc21
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-29 11:36:37 +0100

    Update map.ts

--- src/math/map.ts
+++ src/math/map.ts
@@ -1,13 +1,12 @@
 /**
  * This function maps a value from one range to another range. This can be used for example to
  * calculate a percentage from a value.
- * 
- * @function
+ *
  * @param value The number to map
- * @param fromMin The lower bound of the value’s current range
- * @param fromMax The upper bound of the value’s current range
- * @param toMin The lower bound of the value’s target range
- * @param toMax The upper bound of the value’s target range
+ * @param fromMin The lower bound of the value's current range
+ * @param fromMax The upper bound of the value's current range
+ * @param toMin The lower bound of the value's target range
+ * @param toMax The upper bound of the value's target range
  * @returns The mapped value
  */
 export default function map(
