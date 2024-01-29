4c06ac8958a831b087c675091024445f02c46499
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-29 07:39:55 +0100

    Create split.ts

---
+++ src/filesystem/split.ts
@@ -0,0 +1,13 @@
+/**
+ * This function takes a path and a separator and returns a separated string array, containing the single path elements.
+ *
+ * @param path The path to separate
+ * @param separator The separator used for the path
+ * @returns The separated path
+ */
+export default function split(
+    path : string,
+    separator : string | RegExp
+) : string[] {
+    return path.split(separator);
+}
