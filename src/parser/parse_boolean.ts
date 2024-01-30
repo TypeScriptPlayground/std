f71d5fea68ca1ce84543b653fb26b8e5fb722dc3
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-30 13:27:51 +0100

    Updated JSDoc

--- src/parser/parse_boolean.ts
+++ src/parser/parse_boolean.ts
@@ -1,6 +1,7 @@
 /**
  * This function converts the provided string boolean to an actual boolean type.
  * 
+ * @param booleanString The boolean string
  * @returns The boolean value of the string
  */
 export default function parseBoolean(booleanString : string) : boolean {
