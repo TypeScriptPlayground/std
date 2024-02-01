321cd6260d80a817a2e91bca3e50353ad5b414a5
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-01 11:11:49 +0100

    Update trim_characters.ts

--- src/format/trim_characters.ts
+++ src/format/trim_characters.ts
@@ -1,7 +1,17 @@
+import trimCharactersEnd from "./trim_characters_end.ts";
+import trimCharactersStart from "./trim_characters_start.ts";
+
+/**
+ * This function is similar to the standard `String.prototype.trim()` function. With this function, several characters
+ * can be specified as a string which are then trimmed at the start and end of the `inputString`.
+ * 
+ * @param inputString The input string to trim the start and end
+ * @param characters The characters to trim
+ * @returns The trimmed input string
+ */
 export default function trimCharacters(
     inputString : string,
     characters : string
 ) : string {
-    const charactersArray = characters.split('');
-
+    return trimCharactersStart(trimCharactersEnd(inputString, characters), characters)
 }
