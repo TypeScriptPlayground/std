c90891aa0ff7b8e59c7765279f283fe5af3f644e
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-01 11:14:03 +0100

    Update mod.ts

--- src/format/mod.ts
+++ src/format/mod.ts
@@ -1,7 +1,13 @@
 import formatString from "./format_string.ts";
 import truncate from "./truncate.ts";
+import trimCharactersStart from "./trim_characters_start.ts";
+import trimCharactersEnd from "./trim_characters_end.ts";
+import trimCharacters from "./trim_characters.ts";
 
 export { 
     formatString,
-    truncate
+    truncate,
+    trimCharactersStart,
+    trimCharactersEnd,
+    trimCharacters
 }
