bc7d2c35a6a8e3e255e7569bb3a23ee39e64aefc
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-01 09:06:02 +0100

    Update remove_duplicates.ts

--- src/format/remove_duplicates.ts
+++ src/format/remove_duplicates.ts
@@ -1,3 +1,19 @@
-export default function removeDuplicates<ArrayType extends string | number>(array : ArrayType[]) : ArrayType[] {
+/**
+ * This function removes duplicates in an array. This function does not work for objects.
+ * 
+ * @example
+ * ```ts
+ * console.log(removeDuplicates([1, 2, 3, 4, 3, 5, 4]));
+ * // [1, 2, 3, 4, 5]
+ * 
+ * console.log(removeDuplicates(['Apple', 'Banana', 'Banana', 'Cherry', 'Apple']));
+ * // ['Apple', 'Banana', 'Cherry']
+ * ```
+ * @param array The array from where to remove duplicates from
+ * @returns The array without duplicated items
+ */
+export default function removeDuplicates<
+    ArrayType extends number | string | undefined
+>(array : ArrayType[]) : ArrayType[] {
     return [...new Set(array)];
 }
