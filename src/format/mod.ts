833df14db1468cfe8120acc2fa655fc899224f66
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-02 23:25:43 +0100

    Update mod.ts

--- src/format/mod.ts
+++ src/format/mod.ts
@@ -3,11 +3,19 @@ import truncate from "./truncate.ts";
 import trimCharactersStart from "./trim_characters_start.ts";
 import trimCharactersEnd from "./trim_characters_end.ts";
 import trimCharacters from "./trim_characters.ts";
+import trimSequenceStart from './trim_sequence_start.ts';
+import trimSequenceEnd from './trim_sequence_end.ts';
+import trimSequence from './trim_sequence.ts';
+import reverseString from './reverse_string.ts';
 
 export { 
     formatString,
     truncate,
     trimCharactersStart,
     trimCharactersEnd,
-    trimCharacters
+    trimCharacters,
+    trimSequenceStart,
+    trimSequenceEnd,
+    trimSequence,
+    reverseString
 }
