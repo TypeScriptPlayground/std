241cb470e0c9bb2b3a7116b86af4a7861eef544d
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-03 00:28:09 +0100

    Update mod.ts

--- src/logging/mod.ts
+++ src/logging/mod.ts
@@ -1,5 +1,9 @@
+import Logger from './logger.ts';
+import { LoggerOptions } from './logger_options.ts';
 import { StdOutFunctionType } from './std_out_function_type.ts';
 
 export type {
-  StdOutFunctionType
+  StdOutFunctionType,
+  LoggerOptions,
+  Logger
 }
