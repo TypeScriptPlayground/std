566a5c9398148e5479bc9a3807919843c944b5cc
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-06 15:14:39 +0100

    Update for_each_pair.test.ts

--- src/array/for_each_pair.test.ts
+++ src/array/for_each_pair.test.ts
@@ -1,4 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import {
+  assertSpyCall,
+  assertSpyCalls,
+  spy,
+} from "https://deno.land/std@0.214.0/testing/mock.ts";
 import forEachPair from "./for_each_pair.ts";
 
 Deno.test(
