2775316790bbe80e69bce2acbacf830a6b0fc10b
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-06 14:08:33 +0100

    Create forEachPair.ts

---
+++ src/array/forEachPair.ts
@@ -0,0 +1,44 @@
+/**
+ * This function calls the specified `pairFunction` for all the elements in an array. The function
+ * starts with the second element and calls the `pairFunction` for every element. If the array only
+ * contains one element, the `pairFunction` does not get called.
+ * 
+ * @example
+ * ```ts
+ * forEachPair(
+ *   [1, 2, 3, 4],
+ *   (previousValue, currentValue) => {console.log(previousValue, currentValue)}
+ * )
+ * // 1 2
+ * // 2 3
+ * // 3 4
+ * ```
+ * @param array The array to loop over
+ * @param pairFunction A function that the method calls for each element in the array
+ */
+export default function forEachPair<ArrayType>(
+  array : ArrayType[],
+  pairFunction : (
+    previousValue : ArrayType,
+    currentValue : ArrayType,
+    currentIndex : number,
+    array : ArrayType[]
+  ) => void
+) : void {
+  array.reduce(
+    (
+      previousValue,
+      currentValue,
+      currentIndex,
+      array
+    ) => {
+      pairFunction(
+        previousValue,
+        currentValue,
+        currentIndex,
+        array
+      );
+      return currentValue
+    }
+  )
+}
