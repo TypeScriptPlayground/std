88b4b35abb5013a602c40dfb0492633bc74d5014
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-07 14:34:10 +0100

    Update for_each_pair.test.ts

--- src/array/for_each_pair.test.ts
+++ src/array/for_each_pair.test.ts
@@ -13,12 +13,37 @@ Deno.test(
             name: 'Empty array',
             fn: () => {
                 function dummyFunction(
-                    previousValue,
-                    currentValue
-                ) {}
+                    previousValue : number,
+                    currentValue : number
+                ) : void {}
+                
                 const dummyFunctionSpy = spy(dummyFunction);
 
-                
+                forEachPair(
+                    [0, 2, 3, 7],
+                    dummyFunctionSpy
+                )
+
+                assertSpyCall(dummyFunctionSpy, 0, {
+                    args: [
+                        0,
+                        2
+                    ]
+                })
+                assertSpyCall(dummyFunctionSpy, 1, {
+                    args: [
+                        2,
+                        3
+                    ]
+                })
+                assertSpyCall(dummyFunctionSpy, 2, {
+                    args: [
+                        3,
+                        7
+                    ]
+                })
+
+                assertSpyCalls(dummyFunctionSpy, 3);
             }
         })
     }
