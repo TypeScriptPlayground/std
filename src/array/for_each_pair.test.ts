e28bd6b5cd9c04e28cbce54aa85d5e202edf2111
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-13 14:51:36 +0100

    Update for_each_pair.test.ts

--- src/array/for_each_pair.test.ts
+++ src/array/for_each_pair.test.ts
@@ -7,43 +7,38 @@ import forEachPair from "./for_each_pair.ts";
 
 Deno.test(
   'Call function for each pair.',
-  async (test) => {
-    await test.step({
-      name: 'Empty array',
-      fn: () => {
-        function dummyFunction(
-          previousValue : number,
-          currentValue : number
-        ) : void {}
-        
-        const dummyFunctionSpy = spy(dummyFunction);
+  () => {
+    function dummyFunction(
+      previousValue : number,
+      currentValue : number
+    ) : void {}
+    
+    const dummyFunctionSpy = spy(dummyFunction);
 
-        forEachPair(
-          [0, 2, 3, 7],
-          dummyFunctionSpy
-        )
+    forEachPair(
+      [0, 2, 3, 7],
+      dummyFunctionSpy
+    )
 
-        assertSpyCall(dummyFunctionSpy, 0, {
-          args: [
-            0,
-            2
-          ]
-        })
-        assertSpyCall(dummyFunctionSpy, 1, {
-          args: [
-            2,
-            3
-          ]
-        })
-        assertSpyCall(dummyFunctionSpy, 2, {
-          args: [
-            3,
-            7
-          ]
-        })
-
-        assertSpyCalls(dummyFunctionSpy, 3);
-      }
+    assertSpyCall(dummyFunctionSpy, 0, {
+      args: [
+        0,
+        2
+      ]
     })
+    assertSpyCall(dummyFunctionSpy, 1, {
+      args: [
+        2,
+        3
+      ]
+    })
+    assertSpyCall(dummyFunctionSpy, 2, {
+      args: [
+        3,
+        7
+      ]
+    })
+
+    assertSpyCalls(dummyFunctionSpy, 3);
   }
 )
