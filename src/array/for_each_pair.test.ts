576993b212900ada5fa699dd0ae1e3e8525c131c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-13 15:20:42 +0100

    Update for_each_pair.test.ts

--- src/array/for_each_pair.test.ts
+++ src/array/for_each_pair.test.ts
@@ -7,40 +7,90 @@ import forEachPair from "./for_each_pair.ts";
 
 Deno.test(
   'Call function for each pair.',
-  () => {
+  async (test) => {
     function dummyFunction(
       previousValue : number,
-      currentValue : number
-    ) : void {}
+      currentValue : number,
+      currentIndex: number, 
+      array: number[]
+    ) : {
+      previousValue : number,
+      currentValue : number,
+      currentIndex: number, 
+      array: number[]
+    } {
+      return {
+        previousValue,
+        currentValue,
+        currentIndex,
+        array
+      }
+    }
     
     const dummyFunctionSpy = spy(dummyFunction);
 
     forEachPair(
-      [0, 2, 3, 7],
+      [
+        0,
+        2, 
+        3, 
+        7
+      ],
       dummyFunctionSpy
     )
-
-    assertSpyCall(dummyFunctionSpy, 0, {
-      args: [
-        0,
-        2
-      ]
+    
+    await test.step({
+      name: 'Function call arguments.',
+      fn: () => {
+        assertSpyCall(dummyFunctionSpy, 0, {
+          args: [
+            0,
+            2,
+            1,
+            [
+              0, 
+              2, 
+              3, 
+              7
+            ]
+          ]
+        })
+    
+        assertSpyCall(dummyFunctionSpy, 1, {
+          args: [
+            2,
+            3,
+            2,
+            [
+              0, 
+              2, 
+              3, 
+              7
+            ]
+          ]
+        })
+    
+        assertSpyCall(dummyFunctionSpy, 2, {
+          args: [
+            3,
+            7,
+            3,
+            [
+              0, 
+              2, 
+              3, 
+              7
+            ]
+          ]
+        })
+      }
     })
 
-    assertSpyCall(dummyFunctionSpy, 1, {
-      args: [
-        2,
-        3
-      ]
+    await test.step({
+      name: 'Function calls.',
+      fn: () => {
+        assertSpyCalls(dummyFunctionSpy, 3);
+      }
     })
-
-    assertSpyCall(dummyFunctionSpy, 2, {
-      args: [
-        3,
-        7
-      ]
-    })
-
-    assertSpyCalls(dummyFunctionSpy, 3);
   }
 )
