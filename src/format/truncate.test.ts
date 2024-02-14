2d73318cbb98e102e61f51059a047328e7dce2a0
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-14 13:45:39 +0100

    Formatting

--- src/format/truncate.test.ts
+++ src/format/truncate.test.ts
@@ -1,47 +1,47 @@
-import { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
-import truncate from "./truncate.ts";
+import { assertEquals } from 'https://deno.land/std@0.203.0/assert/assert_equals.ts';
+import truncate from './truncate.ts';
 
 Deno.test(
-    'Truncate string.',
-    async (test) => {
-        await test.step({
-            name: 'string.length <= maxLength',
-            fn: () => {
-                assertEquals(
-                    truncate(
-                        'This is a short string.',
-                        100
-                    ),
-                    'This is a short string.'
-                )
-            }
-        })
-        
-        await test.step({
-            name: 'string.length > maxLength',
-            fn: () => {
-                assertEquals(
-                    truncate(
-                        'This is a very long string that I would like to cut off at a certain length.',
-                        36
-                    ),
-                    'This is a very long string that I...'
-                )
-            }
-        })
+  'Truncate string.',
+  async (test) => {
+    await test.step({
+      name: 'string.length <= maxLength',
+      fn: () => {
+        assertEquals(
+          truncate(
+            'This is a short string.',
+            100,
+          ),
+          'This is a short string.',
+        );
+      },
+    });
 
-        await test.step({
-            name: 'string.length > maxLength with custom ellipsis.',
-            fn: () => {
-                assertEquals(
-                    truncate(
-                        'This is a very long string that I would like to cut off at a certain length.',
-                        32,
-                        ' >>>>>'
-                    ),
-                    'This is a very long string >>>>>'
-                )
-            }
-        })
-    }
-)
+    await test.step({
+      name: 'string.length > maxLength',
+      fn: () => {
+        assertEquals(
+          truncate(
+            'This is a very long string that I would like to cut off at a certain length.',
+            36,
+          ),
+          'This is a very long string that I...',
+        );
+      },
+    });
+
+    await test.step({
+      name: 'string.length > maxLength with custom ellipsis.',
+      fn: () => {
+        assertEquals(
+          truncate(
+            'This is a very long string that I would like to cut off at a certain length.',
+            32,
+            ' >>>>>',
+          ),
+          'This is a very long string >>>>>',
+        );
+      },
+    });
+  },
+);
