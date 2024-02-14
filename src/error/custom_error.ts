aff0b00f4d7500aca812b329787829c10876a81a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-14 13:50:24 +0100

    Update custom_error.ts

--- src/error/custom_error.ts
+++ src/error/custom_error.ts
@@ -3,7 +3,6 @@
  * but takes into account the inheritance of the types.
  * This allows you to check `error instanceof CustomError`.
  * The error class suppresses the correct inheritance of the prototype if you inherit directly from it. 
- * inherits directly from it
  *
  * @param message The custom error message
  * @returns The custom error
