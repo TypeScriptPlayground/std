bb66aa9db77a3b375c71caccad73922683b6033a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-14 13:46:07 +0100

    Update mod.ts

--- src/format/mod.ts
+++ src/format/mod.ts
@@ -2,6 +2,6 @@ import replacePlaceholder from "./replace_placeholder.ts";
 import truncate from "./truncate.ts";
 
 export { 
-    replacePlaceholder,
-    truncate,
+  replacePlaceholder,
+  truncate,
 }
