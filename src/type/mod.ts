acfae5c5322e5e76bac16bc1c40acaf1ffb17614
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-14 14:05:04 +0100

    Update mod.ts

--- src/type/mod.ts
+++ src/type/mod.ts
@@ -2,10 +2,22 @@ import ValuesFromObject from "./values_from_object.ts";
 import ValuesFromArray from "./values_from_array.ts";
 import KeysFromObject from "./keys_from_object.ts";
 import NumberRange from "./number_range.ts";
+import Char from './char.ts';
+import DualByte from './binary/dual_byte.ts';
+import DualDigit from './binary/dual_digit.ts';
+import Dual4Bit from './binary/dual_4bit.ts'
+import HexDigit from './binary/hex_digit.ts';
+import OctalDigit from './binary/octal_digit.ts';
 
 export type {
     ValuesFromObject,
     ValuesFromArray,
     KeysFromObject,
-    NumberRange
+    NumberRange,
+    Char,
+    DualByte,
+    DualDigit,
+    Dual4Bit,
+    HexDigit,
+    OctalDigit
 }
