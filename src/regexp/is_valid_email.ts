c15c8c2fde10c6bdd8102da02da2ae7097705a3e
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-18 16:12:39 +0100

    Update is_valid_email.ts

--- src/regexp/is_valid_email.ts
+++ src/regexp/is_valid_email.ts
@@ -1,11 +1,20 @@
 /**
  * This function checks if the input string is a valid email address.
  * 
- * @param input The email string
+ * [RFC 822](https://pdw.ex-parrot.com/Mail-RFC822-Address.html) |
+ * [RFC 5322](https://datatracker.ietf.org/doc/html/rfc5322)
+ * 
+ * @param input The email address string
  * @returns If the email is valid
  */
 export default function isValidEmail(input : string) : boolean {
-  // deno-lint-ignore no-control-regex
-  const matchEmailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  const matchEmailPattern = new RegExp([
+    '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21',
+    '\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+',
+    '[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4]',
+    '[0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|',
+    '\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])'
+  ].join(''));
+
   return matchEmailPattern.test(input.trim());
 }
