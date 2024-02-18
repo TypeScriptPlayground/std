bc8cf44e6971b53d1daad77b9ad57db04d77ab1b
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-02-18 16:24:14 +0100

    Update mod.ts

--- src/regexp/mod.ts
+++ src/regexp/mod.ts
@@ -1,5 +1,9 @@
+import isValidEmail from './is_valid_email.ts';
+import isValidUrl from './is_valid_url.ts';
 import parseString from "./parse_string.ts";
 
 export const regexp = {
+  isValidEmail,
+  isValidUrl,
   parseString
 }
