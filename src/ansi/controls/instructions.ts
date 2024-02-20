55c4306c5ba0a924a75efc0bdc1cccacf0cf9855
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-20 14:19:45 +0100

    Update instructions.ts

--- src/ansi/controls/instructions.ts
+++ src/ansi/controls/instructions.ts
@@ -4,19 +4,19 @@ import type { ValuesFromObject } from "../../type/mod.ts";
  * This object contains all instruction ANSI control codes.
  */
 const instructions = {
-  A: 'A',
-  B: 'B',
-  C: 'C',
-  D: 'D',
-  E: 'E',
-  F: 'F',
-  G: 'G',
-  H: 'h',
-  I: 'I',
-  J: 'J',
-  K: 'K',
-  M: 'm',
-  S: 's',
+  CURSOR_UP_TERMINATOR: 'A',
+  CURSOR_DOWN_TERMINATOR: 'B',
+  CURSOR_RIGHT_TERMINATOR: 'C',
+  CURSOR_LEFT_TERMINATOR: 'D',
+  CURSOR_DOWN_BEGIN_TERMINATOR: 'E',
+  CURSOR_UP_BEGIN_TERMINATOR: 'F',
+  CURSOR_TO_COLUMN_TERMINATOR: 'G',
+  DEC_SET: 'h',
+  SCREEN_ERASE: 'J',
+  SCREEN_FROM_CURSOR_TO_END_ERASE: 'K',
+  DEC_RESET: 'l',
+  FORMAT_TERMINATOR: 'm',
+  CURSOR_SAVE_POSITION: 's',
   U: 'u',
   PRIVATE: '?'
 } as const;
