ac7e74018112b35fbbb55ce4e6a3ed196dadf81a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-02-27 13:32:08 +0100

    Refactoring

--- src/ansi/colors/background/black.ts
+++ src/ansi/colors/background/black.ts
@@ -1,19 +1,6 @@
-import controls from '../../controls/mod.ts';
-import formats from '../../formats/mod.ts';
-import backgroundColors from '../4bit/background_colors.ts';
 
 export default function black(
   text? : string
 ) : string {
-  if (!text) {
-    return controls.c1Controls.CSI + backgroundColors.BLACK + 'm';
-  } else {
-    return controls.c1Controls.CSI +
-      backgroundColors.RED +
-      'm' +
-      text +
-      controls.c1Controls.CSI +
-      formats.colors.BACKGROUND_COLOR_DEFAULT + 
-      'm';
-  }
+  return ''
 }
