e4d2fbd51723d98a6b9257682f015439ed8aa11a
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-03-01 11:15:24 +0100

    Update mod.ts

--- src/ansi/mod.ts
+++ src/ansi/mod.ts
@@ -1,78 +1,19 @@
-import {
-  bit4BackgroundColors,
-  bit4ForegroundColors,
-  bit8Colors,
-  bit8GrayscaleColors
-} from './colors/mod.ts'
-import {
-  c0Controls,
-  c1Controls,
-  instructions
-} from './controls/mod.ts'
-import {
-  
-} from "./formats/mod.ts";
+import * as color from './colors/mod.ts'
+import * as control from './controls/mod.ts'
+import * as format from './formats/mod.ts';
 
-import {
-  Bit4BackgroundColor,
-  Bit4ForegroundColor,
-  Bit8ColorfulColor,
-  Bit8GrayscaleColor,
-  Bit24Color
-} from './colors/mod.ts'
-import { 
-  C0Control,
-  C1Control,
-  Instruction
-} from "./controls/mod.ts";
-import { 
-  Color,
-  Font,
-  Action,
-  Ideogram,
-  Script,
-  TextDecoration
-} from "./formats/mod.ts";
+import * as Color from './colors/mod.ts'
+import * as Control from './controls/mod.ts'
+import * as Format from './formats/mod.ts'
 
-export const ansi = {
-  colors,
-  controls,
-  formats
+export {
+  color,
+  control,
+  format
 }
 
 export type {
-    Bit4BackgroundColor,
-    Bit4ForegroundColor,
-    Bit8ColorfulColor,
-    Bit8GrayscaleColor,
-    Bit24Color,
-    C0Control,
-    C1Control,
-    Control,
-    Color,
-    Font,
-    Action,
-    Ideogram,
-    Script,
-    TextDecoration
+  Color,
+  Control,
+  Format
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-console.log(red('Hello World!', 'FOREGROUND'))
-console.log(red('Hello World!', 'BACKGROUND'))
-console.log(red('Hello World!', 'BOTH'))
-
-console.log(whiteForeground() + redBackground() + 'Hello World!');
-
-red(str)
