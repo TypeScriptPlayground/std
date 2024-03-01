86756110dd46c7bbee8d916ca846af3b4d25a5bb
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-03-01 10:02:25 +0100

    Update mod.ts

--- src/ansi/colors/mod.ts
+++ src/ansi/colors/mod.ts
@@ -5,11 +5,11 @@ import bit8GrayscaleColors from './8bit/grayscale_colors.ts';
 
 import { BackgroundColor as Bit4BackgroundColor } from './4bit/background_colors.ts';
 import { ForegroundColor as Bit4ForegroundColor } from './4bit/foreground_colors.ts';
-import { ColorfulColor as Bit8Color } from './8bit/colorful_colors.ts';
+import { ColorfulColor as Bit8ColorfulColor } from './8bit/colorful_colors.ts';
 import { GrayscaleColor as Bit8GrayscaleColor } from './8bit/grayscale_colors.ts';
 import { Color as Bit24Color } from './24bit/color.ts';
 
-export default {
+export {
   bit4BackgroundColors,
   bit4ForegroundColors,
   bit8Colors,
@@ -19,7 +19,7 @@ export default {
 export type {
   Bit4BackgroundColor,
   Bit4ForegroundColor,
-  Bit8Color,
+  Bit8ColorfulColor,
   Bit8GrayscaleColor,
   Bit24Color,
 }
