91fc95f7b3b4bd202d9902c47a1172327b9a351d
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-03-01 09:36:31 +0100

    Refactoring

--- src/ansi/colors/4bit/generate_color_text.ts
+++
@@ -1,9 +0,0 @@
-import Color from './color.ts';
-import generateColor from './generate_color.ts'
-
-export default function generateColorText(
-  color : Color,
-  text : string
-) : string {
-  return generateColor(color) + text + generateColor()
-}
--- src/ansi/colors/background/black.ts
+++ src/ansi/colors/background/black.ts
@@ -1,12 +1,9 @@
-import {c0Controls} from '../../controls/mod.ts';
-import backgroundColors from '../4bit/background_colors.ts';
-import generateColor from '../4bit/generate_color.ts';
 
 export default function black(
   text? : string
 ) : string {
   if (!text) {
-    return generateColor(backgroundColors.BLACK)
+    return ''
   }
-  return generateColor(backgroundColors.BLACK) + text
+  return ''
 }
