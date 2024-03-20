91926fd094f1e6924307f570868bd94c6462b049
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-03-20 19:59:01 +0100

    Update mod.ts

--- src/ansi/mod.ts
+++ src/ansi/mod.ts
@@ -1,15 +1,15 @@
-import * as color from './colors/mod.ts'
-import * as control from './controls/mod.ts'
-import * as format from './formats/mod.ts';
+import * as colors from './colors/mod.ts'
+import * as controls from './controls/mod.ts'
+import * as formats from './formats/mod.ts';
 
 import * as Color from './colors/mod.ts'
 import * as Control from './controls/mod.ts'
 import * as Format from './formats/mod.ts'
 
 export const ansi = {
-  color,
-  control,
-  format
+  colors,
+  controls,
+  formats
 }
 
 export type {
