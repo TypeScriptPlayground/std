a890912a584a5c64fc04675098dd6974b99099f1
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-04-18 19:57:42 +0200

    Update capitalize_word.ts

--- src/string/capitalize_word.ts
+++ src/string/capitalize_word.ts
@@ -1,3 +1,17 @@
+/**
+ * This function capitalizes a word.
+ * 
+ * @example
+ * ```
+ * console.log(capitalizeWord('hello'))
+ * // Hello
+ * 
+ * console.log(capitalizeWord('wOrLd'))
+ * // World
+ * ```
+ * @param inputString The string to reverse
+ * @returns The reversed string
+ */
 export function capitalizeWord(word : string) : string {
   return word ? word?.[0]?.toLocaleUpperCase() + word.slice(1).toLocaleLowerCase() : word;
 }
