beb7ffa30b63e4ad358d0b80bf5db7d58856e814
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-04-22 19:09:23 +0200

    Update custom_error.ts

--- src/error/custom_error.ts
+++ src/error/custom_error.ts
@@ -14,6 +14,6 @@ export default class CustomError extends Error {
     options? : ErrorOptions
   ) {
     super(message, options);
-    Object.setPrototypeOf( this, new.target.prototype );
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
