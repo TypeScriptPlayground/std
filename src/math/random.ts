ec4c508761f4eb3d548c5e389b424fcfbb4461fd
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-06-17 09:49:47 +0200

    Update random.ts

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,6 +1,14 @@
+/**
+ * This function generates a random number between `min` and `max`.
+ * @param min Minimum number
+ * @param max Maximum number
+ * @param process A function the processes the random generated number
+ * @returns A random number
+ */
 export default function random(
   min : number,
-  max : number
-) {
-  
+  max : number,
+  process = Math.floor
+) : number {
+  return 0;
 }
