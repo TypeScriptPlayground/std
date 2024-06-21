97f6a4e6550de9f4b3e3443f634c430aea56fbfe
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-06-21 07:17:15 +0200

    Update random.test.ts

--- src/math/random.test.ts
+++ src/math/random.test.ts
@@ -1,1 +1,34 @@
+import { assertGreaterOrEqual } from "https://deno.land/std@0.224.0/assert/assert_greater_or_equal.ts";
+import { assertLessOrEqual } from "https://deno.land/std@0.224.0/assert/assert_less_or_equal.ts";
+import random from './random.ts';
 
+Deno.test(
+  'Generate a random number.',
+  async (test) => {
+    await test.step({
+      name: 'Number is not below min.',
+      fn: () => {
+        assertGreaterOrEqual(
+          random(
+            10,
+            20,
+          ),
+          10,
+        );
+      },
+    });
+
+    await test.step({
+      name: 'Number is not above max.',
+      fn: () => {
+        assertLessOrEqual(
+          random(
+            10,
+            20,
+          ),
+          20,
+        );
+      },
+    });
+  },
+);
