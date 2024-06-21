c0876618108d78c91a781d243df1b80765b9be0d
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-06-21 07:10:09 +0200

    Update random.ts

--- src/math/random.ts
+++ src/math/random.ts
@@ -10,7 +10,10 @@ export default function random(max : number) : number;
  * @param max Maximum number
  * @returns A random number
  */
-export default function random(min : number, max : number) : number;
+export default function random(
+  min : number,
+  max : number
+) : number;
 export default function random(...parameters : number[]) : number {
   if (parameters.length === 1) {
     const [max] = parameters
