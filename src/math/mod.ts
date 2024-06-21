5897c2142c08e4f5f6c2034e3772ab3ec710bf39
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-06-21 07:11:38 +0200

    Update mod.ts

--- src/math/mod.ts
+++ src/math/mod.ts
@@ -2,10 +2,12 @@ import average from "./average.ts";
 import map from "./map.ts";
 import median from "./median.ts";
 import toFixed from "./to_fixed.ts";
+import random from "./random.ts"
 
 export const math = { 
   average,
   map,
   median,
-  toFixed
+  toFixed,
+  random
 }
