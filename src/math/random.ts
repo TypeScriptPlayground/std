2cd8706f8657c40e121d029a7378812e51405abf
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-06-21 07:09:48 +0200

    Update random.ts

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,10 +1,15 @@
 /**
+ * This function generates a random number between `0` and `max`.
+ * @param min Minimum number
+ * @returns A random number
+ */
+export default function random(max : number) : number;
+/**
  * This function generates a random number between `min` and `max`.
  * @param min Minimum number
  * @param max Maximum number
  * @returns A random number
  */
-export default function random(max : number) : number;
 export default function random(min : number, max : number) : number;
 export default function random(...parameters : number[]) : number {
   if (parameters.length === 1) {
