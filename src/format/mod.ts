c527aea910c8c7aaddbaf0d00e458a2dc7481a59
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-07-22 09:29:59 +0200

    Update mod.ts

--- src/format/mod.ts
+++ src/format/mod.ts
@@ -1,7 +1,9 @@
 import replacePlaceholder from "./replace_placeholder.ts";
 import truncate from "./truncate.ts";
+import joinAnd from "./join_and.ts"
 
 export const format = { 
   replacePlaceholder,
   truncate,
+  joinAnd
 }
