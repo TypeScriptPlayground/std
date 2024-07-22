4bb0b25308760e9602245d89d7c9ec080644dbc8
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-07-22 09:08:09 +0200

    Update mod.ts

--- src/string/mod.ts
+++ src/string/mod.ts
@@ -1,11 +1,9 @@
 import reverseString from './reverse_string.ts';
 import trim from './trim/mod.ts'
-import capitalizeWord from './capitalize_word.ts'
 import splitBetweenTokens from './split_between_tokens.ts'
 
 export const string = {
   reverseString,
   trim,
-  capitalizeWord,
   splitBetweenTokens
 }
