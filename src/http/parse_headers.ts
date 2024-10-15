153e527da73375f135b3f580c1f21c7528c7b4f8
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-10-15 16:31:29 +0200

    Update parse_headers.ts

--- src/http/parse_headers.ts
+++ src/http/parse_headers.ts
@@ -1,11 +1,28 @@
+type Keys =
+  | "accept"
+  | "accept-encoding"
+  | "accept-language"
+  | "connection"
+  | "host"
+  | "referer"
+  | "sec-ch-ua"
+  | "sec-ch-ua-mobile"
+  | "sec-ch-ua-platform"
+  | "sec-fetch-dest"
+  | "sec-fetch-mode"
+  | "sec-fetch-site"
+  | "user-agent"
+  // deno-lint-ignore ban-types
+  | (string & {})
+
 /**
  * This function parses a Headers object and returns it as a parsed object.
  * 
  * @param headers Header that should get parsed
  * @returns The parsed header
  */
-export default function parseHeaders(headers : Headers) : Record<string, string> {
-  const header : Record<string, string> = {};
+export default function parseHeaders(headers : Headers) : Record<Keys, string> {
+  const header : Record<Keys | string, string> = {};
   for(const [key, value] of headers.entries()) {
     header[key] = value;
   };
