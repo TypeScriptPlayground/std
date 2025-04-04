8682faec793df74a7bfb481b48c5fc447128e86d
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-04-04 19:15:11 +0200

    Update union_to_array_union.ts

--- src/type/union_to_array_union.ts
+++ src/type/union_to_array_union.ts
@@ -1,6 +1,12 @@
 /**
  * This type creates an array of each type in the Union.
  * 
+ * @example
+ * ```ts
+ * type MyUnion = string | number | number[];
+ * type MyArray = UnionToArrayUnion<MyUnion>;
+ * // type MyArray = string[] | number[] | number[][];
+ * ```
  * @template Type The type of the union from which to create a new array type
  */
 // deno-lint-ignore no-explicit-any
