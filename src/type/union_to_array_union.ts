c3da08878873aac72e6ab789fcb2bedf0ec32adc
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-04-04 19:07:35 +0200

    Update union_to_array_union.ts

--- src/type/union_to_array_union.ts
+++ src/type/union_to_array_union.ts
@@ -1,4 +1,7 @@
 /**
- * This type
+ * This type creates an array of each type in the Union.
+ * 
+ * @template Type The type of the union from which to create a new array type
  */
+// deno-lint-ignore no-explicit-any
 export type UnionToArrayUnion<Type> = Type extends any ? Type[] : never;
